e2f769d117fe9accf7318b7a341da4e57469fd5a
Liquid Monks <info@liquidmonks.com>
2023-06-03 11:51:29 -0500

    server.js includes basic function blocks for typeDefs and resolvers.

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 import { ApolloServer, gql } from "apollo-server";
 
+
+const typeDefs = gql`
+ type Query {
+    greet: String
+ }
+`;
+
+const resolvers = {
+    Query: {
+        greet: () => 'Hello World!'
+}
+
+
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
